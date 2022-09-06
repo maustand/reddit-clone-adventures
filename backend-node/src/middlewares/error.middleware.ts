5f876359bfc3ab3c7001d250d2bee00eadf80227
Mauricio Stand <mau.stand@gmail.com>
2022-09-06 15:26:18 +0300

    changes folder structure

--- backend-node/src/middlewares/error.middleware.ts
+++ backend-node/src/middlewares/error.middleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { HttpException } from '../utils/httpException/HttpException';
+import { HttpException } from '../utils/httpException';
 import { logger } from '../utils/logger';
 
 const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
